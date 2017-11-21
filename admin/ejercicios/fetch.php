6655503e393349bdb9aa976f2d51352916382750
Ricardo Pérez <ricardojpf98@gmail.com>
2017-11-21 10:33:09 -0600

    Added fetch of ejercicios

--- admin/ejercicios/fetch.php
+++ admin/ejercicios/fetch.php
@@ -3,7 +3,7 @@
 $connect = mysqli_connect("sql9.freemysqlhosting.net", "sql9206193", "yDz3nenNM4", "sql9206193");
  if(isset($_POST["employee_id"]))
  {
-      $query = "SELECT * FROM cliente WHERE id = '".$_POST["employee_id"]."'";
+      $query = "SELECT * FROM ejercicio WHERE id = '".$_POST["employee_id"]."'";
       $result = mysqli_query($connect, $query);
       $row = mysqli_fetch_array($result);
       echo json_encode($row);
