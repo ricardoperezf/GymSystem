c8c1d8493b90166172531a5b561ecf9bf1d7e7f7
Ricardo Pérez <ricardojpf98@gmail.com>
2017-11-21 09:54:20 -0600

    Added ejercicio's section inside of clientes section and ejercicios section.

--- admin/clientes/clientes.php
+++ admin/clientes/clientes.php
@@ -75,25 +75,11 @@ require_once '../../php/conexion.php';
                             </li>
                         </ul>
                     </li>
-                    <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Example Pages">
-                        <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseExamplePages" data-parent="#exampleAccordion">
+                    <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Charts">
+                        <a class="nav-link" href="../ejercicios/ejercicios.php">
                             <i class="fa fa-fw fa-file"></i>
                             <span class="nav-link-text">Ejercicios</span>
                         </a>
-                        <ul class="sidenav-second-level collapse" id="collapseExamplePages">
-                            <li>
-                                <a href="../login.html">Login Page</a>
-                            </li>
-                            <li>
-                                <a href="../register.html">Registration Page</a>
-                            </li>
-                            <li>
-                                <a href="../forgot-password.html">Forgot Password Page</a>
-                            </li>
-                            <li>
-                                <a href="../blank.html">Blank Page</a>
-                            </li>
-                        </ul>
                     </li>
                 </ul>
                 <ul class="navbar-nav sidenav-toggler">
